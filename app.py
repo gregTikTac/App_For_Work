f396217c5490949bcfbcd85f9f214b3e15fb38e0
Владимир <skyfallagain009@gmail.com>
2022-07-12 14:02:57 +0300

    сделал добавление фамилии к ог, требует рефакторинга...

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-import os
-import sys
 import pandas as pd
 import numpy as np
 from docxtpl import DocxTemplate
 import openpyxl
-
-os.chdir(sys.path[0])
 
 doc = DocxTemplate('probe.docx')
 
@@ -32,4 +28,66 @@ dict_of_lst_og = {}
 for index, value in enumerate(lst):
     dict_of_lst_og[index] = value
     # dict = {index: value for index, value in enumerate(lst} ---> то же самое
+
+print(dict_of_lst_og)
+sog = int(input("Выберите Старшего(нажмите соотвествующую цифру): "))
+
+if sog == 0:
+    context = {
+        'sog': dict_of_lst_og[0]
+    }
+    doc.render(context)
+    doc.save('probe.docx')
+
+elif sog == 1:
+    context = {
+        'sog': dict_of_lst_og[1]
+    }
+    doc.render(context)
+    doc.save('probe.docx')
+
+elif sog == 2:
+    context = {
+        'sog': dict_of_lst_og[2]
+    }
+    doc.render(context)
+    doc.save('probe.docx')
+
+elif sog == 3:
+    context = {
+        'sog': dict_of_lst_og[3]
+    }
+    doc.render(context)
+    doc.save('probe.docx')
+
+elif sog == 4:
+    context = {
+        'sog': dict_of_lst_og[4]
+    }
+    doc.render(context)
+    doc.save('probe.docx')
+
+elif sog == 5:
+    context = {
+        'sog': dict_of_lst_og[5]
+    }
+    doc.render(context)
+    doc.save('probe.docx')
+
+elif sog == 6:
+    context = {
+        'sog': dict_of_lst_og[6]
+    }
+    doc.render(context)
+    doc.save('probe.docx')
+
+elif sog == 7:
+    context = {
+        'sog': dict_of_lst_og[7]
+    }
+    doc.render(context)
+    doc.save('probe.docx')
+
+else:
+    print('Такого значения нет')
 
