0a92321fb105bd3cc438b7a810c76ff501c315fd
Владимир <skyfallagain009@gmail.com>
2022-07-12 20:34:15 +0300

    рефакторинг: Добавили функцию изменения страших в записи

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 from docxtpl import DocxTemplate
 import openpyxl
 
-doc = DocxTemplate('probe.docx')
+doc = DocxTemplate('probe2.docx')
 
 # import database from exel
 # data = pd.read_excel('word_automation.xlsm',
@@ -28,66 +28,25 @@ dict_of_lst_og = {}
 for index, value in enumerate(lst):
     dict_of_lst_og[index] = value
     # dict = {index: value for index, value in enumerate(lst} ---> то же самое
-
+#
 print(dict_of_lst_og)
 sog = int(input("Выберите Старшего(нажмите соотвествующую цифру): "))
 
-if sog == 0:
-    context = {
-        'sog': dict_of_lst_og[0]
-    }
-    doc.render(context)
-    doc.save('probe.docx')
 
-elif sog == 1:
-    context = {
-        'sog': dict_of_lst_og[1]
-    }
-    doc.render(context)
-    doc.save('probe.docx')
 
-elif sog == 2:
-    context = {
-        'sog': dict_of_lst_og[2]
-    }
-    doc.render(context)
-    doc.save('probe.docx')
+def change_sog_of_record():
+    """Позволяет менять л/с в записи"""
+    counter = 0
+    while True:
+        if sog == counter:
+            context = {
+                'sog': dict_of_lst_og[counter]
+            }
+            doc.render(context)
+            doc.save('probe2.docx')
+            break
+        else:
+            counter += 1
 
-elif sog == 3:
-    context = {
-        'sog': dict_of_lst_og[3]
-    }
-    doc.render(context)
-    doc.save('probe.docx')
 
-elif sog == 4:
-    context = {
-        'sog': dict_of_lst_og[4]
-    }
-    doc.render(context)
-    doc.save('probe.docx')
-
-elif sog == 5:
-    context = {
-        'sog': dict_of_lst_og[5]
-    }
-    doc.render(context)
-    doc.save('probe.docx')
-
-elif sog == 6:
-    context = {
-        'sog': dict_of_lst_og[6]
-    }
-    doc.render(context)
-    doc.save('probe.docx')
-
-elif sog == 7:
-    context = {
-        'sog': dict_of_lst_og[7]
-    }
-    doc.render(context)
-    doc.save('probe.docx')
-
-else:
-    print('Такого значения нет')
-
+change_sog_of_record()
