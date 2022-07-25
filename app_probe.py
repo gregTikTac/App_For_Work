ca6a81df2d2fb262cb6abc84595073ecb6b305b6
Владимир <skyfallagain009@gmail.com>
2022-07-25 23:32:21 +0300

    пришел к единому формированию шаблона. Осталось добавить изменение даты и месяца, а так же обработка исключений

--- app_probe.py
+++ app_probe.py
@@ -40,7 +40,7 @@ def converting_exel_files_to_list_for_PAT():
         list_of_pat.append(str(row[2].value))
 
 
-def converting_exel_files_to_list_PM_for_PAT():
+def converting_exel_files_to_list_PM_for_OG():
     """Конвертирует данные из 4 столбца таблицы exel в список"""
     for row in sheet.rows:
         list_PM.append(str(row[3].value))
@@ -76,7 +76,7 @@ def print_numb_and_values_list_PAT():
     print('+' + '---------------' * 10 + '+')
 
 
-def print_numb_and_values_list_PM_PAT():
+def print_numb_and_values_list_PM_OG():
     """Вывод списка [номер : значение]"""
     print("Список ПМ ОГ:")
     for numb, values in enumerate(list_PM):
@@ -243,7 +243,7 @@ def choose_PM_from_OG():
         print('+' + '---------------' * 10 + '+')
     else:
         while len(list_of_PM_for_OG_for_template) < 1:
-            list_PAT_for_template(input("Введите корректное значение: "))
+            list_PM_for_template(input("Введите корректное значение: "))
             if len(list_of_PM_for_OG_for_template) >= 1:
                 print(list_of_PM_for_OG_for_template)
                 print('+' + '---------------' * 10 + '+')
@@ -255,7 +255,7 @@ def choose_AK_from_PAT():
         print('+' + '---------------' * 10 + '+')
     else:
         while len(list_of_AK_for_PAT_for_template) < 1:
-            list_PAT_for_template(input("Введите корректное значение: "))
+            list_AK_for_template(input("Введите корректное значение: "))
             if len(list_of_AK_for_PAT_for_template) >= 1:
                 print(list_of_AK_for_PAT_for_template)
                 print('+' + '---------------' * 10 + '+')
@@ -263,7 +263,7 @@ def choose_AK_from_PAT():
 
 def render_template():
 # dict = {index: value for index, value in enumerate(lst}
-    context = {
+    context_list = {
             'og0': list_SOG_and_OG_for_template[0],
             'og1': list_SOG_and_OG_for_template[1],
             'og2': list_SOG_and_OG_for_template[2],
@@ -273,89 +273,131 @@ def render_template():
             'og6': list_SOG_and_OG_for_template[6],
             'og7': list_SOG_and_OG_for_template[7],
             'og8': list_SOG_and_OG_for_template[8],
+            'pat0': list_for_PAT_for_template[0],
+            'pat1': list_for_PAT_for_template[1],
+            'pat2': list_for_PAT_for_template[2],
+            'pat3': list_for_PAT_for_template[3],
+            'pat4': list_for_PAT_for_template[4],
+            'pat5': list_for_PAT_for_template[5],
+            'pat6': list_for_PAT_for_template[6],
+            'pat7': list_for_PAT_for_template[7],
+            'pat8': list_for_PAT_for_template[8],
+            'pat9': list_for_PAT_for_template[9],
+            'pat10': list_for_PAT_for_template[10],
+            'pat11': list_for_PAT_for_template[11],
+            'pat12': list_for_PAT_for_template[12],
+            'pat13': list_for_PAT_for_template[13],
+            'pat14': list_for_PAT_for_template[14],
+            'pat15': list_for_PAT_for_template[15],
+            'pat16': list_for_PAT_for_template[16],
+            'pat17': list_for_PAT_for_template[17],
+            'pat18': list_for_PAT_for_template[18],
+            'pat19': list_for_PAT_for_template[19],
+            'pat20': list_for_PAT_for_template[20],
+            'pat21': list_for_PAT_for_template[21],
+            'pat22': list_for_PAT_for_template[22],
+            'pat23': list_for_PAT_for_template[23],
+            'pat24': list_for_PAT_for_template[24],
+            'pm0': list_of_PM_for_OG_for_template[0],
+            'pm1': list_of_PM_for_OG_for_template[1],
+            'pm2': list_of_PM_for_OG_for_template[2],
+            'pm3': list_of_PM_for_OG_for_template[3],
+            'pm4': list_of_PM_for_OG_for_template[4],
+            'pm5': list_of_PM_for_OG_for_template[5],
+            'pm6': list_of_PM_for_OG_for_template[6],
+            'pm7': list_of_PM_for_OG_for_template[7],
+            'pm8': list_of_PM_for_OG_for_template[8],
+            'ak0': list_of_AK_for_PAT_for_template[0],
+            'ak1': list_of_AK_for_PAT_for_template[1],
+            'ak2': list_of_AK_for_PAT_for_template[2],
+            'ak3': list_of_AK_for_PAT_for_template[3],
+            'ak4': list_of_AK_for_PAT_for_template[4],
+            'ak5': list_of_AK_for_PAT_for_template[5],
+            'ak6': list_of_AK_for_PAT_for_template[6],
+            'ak7': list_of_AK_for_PAT_for_template[7],
+            'ak8': list_of_AK_for_PAT_for_template[8],
+            'ak9': list_of_AK_for_PAT_for_template[9],
+            'ak10': list_of_AK_for_PAT_for_template[10],
+            'ak11': list_of_AK_for_PAT_for_template[11],
+            'ak12': list_of_AK_for_PAT_for_template[12],
+            'ak13': list_of_AK_for_PAT_for_template[13],
+            'ak14': list_of_AK_for_PAT_for_template[14],
+            'ak15': list_of_AK_for_PAT_for_template[15],
+            'ak16': list_of_AK_for_PAT_for_template[16],
+            'ak17': list_of_AK_for_PAT_for_template[17],
+            'ak18': list_of_AK_for_PAT_for_template[18],
+            'ak19': list_of_AK_for_PAT_for_template[19],
+            'ak20': list_of_AK_for_PAT_for_template[20],
+            'ak21': list_of_AK_for_PAT_for_template[21],
+            'ak22': list_of_AK_for_PAT_for_template[22],
+            'ak23': list_of_AK_for_PAT_for_template[23],
+            'ak24': list_of_AK_for_PAT_for_template[24],
         }
-
-    doc.render(context)
-    doc.save('probe3.docx')
-
-
-
-
-    # content_PAT = {str(index): value for index, value in enumerate(list_for_PAT_for_template)}
-    # content_PM = {str(index): value for index, value in enumerate(list_of_PM_for_OG_for_template)}
-    # content_AK = {str(index): value for index, value in enumerate(list_of_AK_for_PAT_for_template)}
-
-    # doc.render(content_PAT)
-    # doc.render(content_PM)
-    # doc.render(content_AK)
-
-
-
+    doc.render(context_list)
 
 # ВЫБОР ОГ СОКРАЩЕННОГО СОСТАВА
-converting_exel_files_to_list_for_sog()
-del list_of_sog[0]
-del list_of_sog[-32:]
-print_numb_and_values_list_SOG()
-choose_person_from_SOG()
-
-converting_exel_files_to_list_for_og()
-del list_of_og[0]
-del list_of_og[-20:]
-print_numb_and_values_list_OG()
-choose_person_from_OG()
-
+# converting_exel_files_to_list_for_sog()
+# del list_of_sog[0]
+# del list_of_sog[-32:]
+# print_numb_and_values_list_SOG()
+# choose_person_from_SOG()
 #
-# # ВЫБОР ОГ ПОЛНОГО СОСТАВА
-print('+' + '---------------' * 10 + '+')
-print('Выберите ОГ полного состава')
-print('+' + '---------------' * 10 + '+')
-print()
-choose_person_from_SOG()
-for chose in range(0, 6):
-    choose_person_from_OG()
-print(list_SOG_and_OG_for_template)
-#
+# converting_exel_files_to_list_for_og()
+# del list_of_og[0]
+# del list_of_og[-20:]
+# print_numb_and_values_list_OG()
+# choose_person_from_OG()
+# # #
+# # #
+# # # # ВЫБОР ОГ ПОЛНОГО СОСТАВА
+# print('+' + '---------------' * 10 + '+')
+# print('Выберите ОГ полного состава')
+# print('+' + '---------------' * 10 + '+')
 # print()
-# print('+' + '---------------' * 10 + '+')
-# print("Выберете ПАТ")
-# print('+' + '---------------' * 10 + '+')
+# choose_person_from_SOG()
+# for chose in range(1, 7):
+#     choose_person_from_OG()
+# print(list_SOG_and_OG_for_template)
+# # # #
+print()
+print('+' + '---------------' * 10 + '+')
+print("Выберете ПАТ")
+print('+' + '---------------' * 10 + '+')
 # #
 # # # ВЫБОР Л/С ДЛЯ ПАТ
 # converting_exel_files_to_list_for_PAT()
 # del list_of_pat[0]
 # del list_of_pat[-15:]
 # print_numb_and_values_list_PAT()
-# choose_person_from_PAT()
-# for choose in range(0, 24):
+# for item in range(1, 27):
 #     choose_person_from_PAT()
-# #
-# #
-# print()
-# print('+' + '---------------' * 10 + '+')
-# print("Выберете ПМ для ОГ")
-# print('+' + '---------------' * 10 + '+')
+
+print()
+print('+' + '---------------' * 10 + '+')
+print("Выберете ПМ для ОГ")
+print('+' + '---------------' * 10 + '+')
 #
 # #
-# # # ВЫбор ПМ для ОГ
-# converting_exel_files_to_list_PM_for_PAT()
+# ВЫбор ПМ для ОГ
+# converting_exel_files_to_list_PM_for_OG()
 # del list_PM[0]
 # del list_PM[-2:]
-# print_numb_and_values_list_PM_PAT()
-# for choose in range(0, 8):
+# print_numb_and_values_list_PM_OG()
+# for item in range(1, 10):
 #     choose_PM_from_OG()
+
+print()
+print('+' + '---------------' * 10 + '+')
+print("Выберете AK для ПАТ")
+print('+' + '---------------' * 10 + '+')
 #
-#
-# print()
-# print('+' + '---------------' * 10 + '+')
-# print("Выберете AK для ПАТ")
-# print('+' + '---------------' * 10 + '+')
-#
-# converting_exel_files_to_list_AK_for_PAT()
-# del list_AK[-8:]
-# print_numb_and_values_list_AK_PAT()
-# for choose in range(0, 27):
-#     choose_AK_from_PAT()
+converting_exel_files_to_list_AK_for_PAT()
+del list_AK[-8:]
+del list_AK[0]
+print_numb_and_values_list_AK_PAT()
+for choose in range(1, 26):
+    choose_AK_from_PAT()
 
 
-render_template()
+# render_template()
+doc.save('probe3.docx')
